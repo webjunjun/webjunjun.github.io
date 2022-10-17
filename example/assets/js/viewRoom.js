b8c658b2853dfc69c2660027917331f564a5f635
webjunjun <meliujun@qq.com>
2022-10-17 14:07:43 +0800

    deploy

---
+++ example/assets/js/viewRoom.js
@@ -0,0 +1,76 @@
+// 准备场景、相机、渲染器、光线
+let scene = null;
+let camera = null;
+let renderer = null;
+let initThree = function () {
+  // 增加一个性能监视器
+  stats = new Stats();
+  stats.domElement.style.position = 'absolute';
+  stats.domElement.style.left = '0px';
+  stats.domElement.style.top = '0px';
+  document.body.appendChild(stats.domElement);
+
+  // 创建渲染器
+  const canvas = document.querySelector('#three_canvas');
+  renderer = new THREE.WebGLRenderer({ canvas });
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setSize(window.innerWidth, window.innerHeight);
+
+  // 创建相机
+  const fov = 75;
+  const aspect = window.innerWidth / window.innerHeight;
+  const near = 0.1;
+  const far = 1000;
+  camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+
+  // 增加一个轨道控制器
+  controls = new THREE.OrbitControls(camera, canvas);
+  controls.enableZoom = false;// 禁止摄像机缩放
+  controls.enablePan = false;// 禁止摄像机移动
+  controls.enableDamping = true;// 开启阻尼设置，给轨道控制器带来重量感
+  controls.rotateSpeed = - 0.25;// 旋转速度
+
+  camera.position.z = 5;
+  controls.update();
+
+  // 创建场景
+  scene = new THREE.Scene();
+}
+
+// 加载纹理 渲染球体
+let cube = null;
+let initSkyBox = function () {
+  const loadManager = new THREE.LoadingManager();
+  const loader = new THREE.TextureLoader(loadManager);
+  // BoxGeometry的纹理遵循右手坐标系，先x，后y，最后z
+  const materials = [
+    new THREE.MeshBasicMaterial({map: loader.load('/example/assets/img/viewRoom/keting_r.jpg')}),
+    new THREE.MeshBasicMaterial({map: loader.load('/example/assets/img/viewRoom/keting_l.jpg')}),
+    new THREE.MeshBasicMaterial({map: loader.load('/example/assets/img/viewRoom/keting_u.jpg')}),
+    new THREE.MeshBasicMaterial({map: loader.load('/example/assets/img/viewRoom/keting_d.jpg')}),
+    new THREE.MeshBasicMaterial({map: loader.load('/example/assets/img/viewRoom/keting_f.jpg')}),
+    new THREE.MeshBasicMaterial({map: loader.load('/example/assets/img/viewRoom/keting_b.jpg')})
+  ];
+  loadManager.onLoad = () => {
+    const geometry = new THREE.BoxGeometry(32, 32, 32);
+    cube = new THREE.Mesh(geometry, materials);
+    cube.geometry.scale(32, 32, -32);
+    scene.add(cube);
+    animate();
+  };
+}
+
+let animate = function () {
+  // 渲染场景和相机
+  renderer.render(scene, camera);
+  // 随动画更新性能监视器
+  stats.update();
+  controls.update();
+
+  // 回调动画
+  requestAnimationFrame(animate);
+}
+
+// 启动方法或其他初始化方法
+initThree();
+initSkyBox();
